d11af09bac2a8cc080bba61b7fa34fb356edf979
Milorad Marković <miloradmarkovic49@gmail.com>
2026-09-12 20:23:01 +0200

    rollback test

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b"Hello from pipeline!")
+        sef.wfile.write(b"Hello from pipeline!")
 
 port = int(os.environ.get("PORT", 8080))
 HTTPServer(("0.0.0.0", port), Handler).serve_forever()
